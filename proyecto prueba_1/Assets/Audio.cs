e4e2b209245d5a0f8a3ddba0e2853ddd93f9b45f
ErikaBeltran1 <erika.beltran@unad.edu.co>
2024-11-06 22:37:35 -0500

    pp1_

---
+++ proyecto prueba_1/Assets/Audio.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class AudioControlScript : MonoBehaviour
+{
+    public AudioSource audioSource;  // Referencia al componente AudioSource
+
+    // Método para reproducir el audio
+    public void PlayAudio()
+    {
+        if (!audioSource.isPlaying) // Solo reproducir si no se está reproduciendo ya
+        {
+            audioSource.Play();
+        }
+    }
+
+    // Método para pausar el audio
+    public void PauseAudio()
+    {
+        if (audioSource.isPlaying) // Pausar solo si se está reproduciendo
+        {
+            audioSource.Pause();
+        }
+    }
+}
---
+++ proyecto prueba_1/Assets/PlayVideo.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+
+public class PlayVideo : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;  // Asigna el Video Player desde el Inspector
+
+    void Start()
+    {
+        videoPlayer.Play();  // Reproduce el video cuando comienza el juego
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))  // Reproduce o pausa el video cuando presionas la tecla "P"
+        {
+            if (videoPlayer.isPlaying)
+                videoPlayer.Pause();
+            else
+                videoPlayer.Play();
+        }
+    }
+}
